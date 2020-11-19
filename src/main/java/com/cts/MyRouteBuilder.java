63d4f1cd3c652570203056dd2a7be19c601a62a4
raza <pathantcs@gmail.com>
2020-11-19 14:47:56 +0530

    first camel project

---
+++ src/main/java/com/cts/MyRouteBuilder.java
@@ -0,0 +1,11 @@
+package com.cts;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyRouteBuilder extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		System.out.println("Hello from Camel Application");
+	}
+}
